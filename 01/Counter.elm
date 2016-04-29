7bfcfda47ab34dca4e4952efa88fd3cb5307d1a5
Francois Lagoueyte <francois.lagoueyte@asso-cocktail.fr>
2016-04-29 17:42:29 +0200

    01 works + clean elm-stuff

--- 01/Counter.elm
+++ 01/Counter.elm
@@ -2,6 +2,8 @@ module Counter where
 
 import Html exposing (..)
 import Html.Attributes exposing (id)
+import Html.Events exposing (onClick)
+
 -- Model
 type alias Model = Int
 
@@ -9,12 +11,18 @@ type alias Model = Int
 type Action = Increment | Decrement
 
 update : Action -> Model -> Model
-update action model = 42
+update action model = 
+    case action of
+        Increment -> model + 1
+        Decrement -> model - 1
 
 -- View
 view : Signal.Address Action -> Model -> Html
-view address action = 
+view address model = 
     div [id "mainId"] 
     [
-        h1 [] [text "Mon premier compteur"]
+        h1 [] [text "Mon premier compteur"],
+        button [onClick address Decrement] [text "-"],
+        span [] [text (toString model)],
+        button [onClick address Increment] [text "+"]
     ]
